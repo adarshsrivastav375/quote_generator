5dfcb22159e921ebad5e4ea0233651b6e05faab6
Adarsh Srivastav <adarshsrivastav.375@gmail.com>
2022-09-19 02:57:28 +0530

    added loader

--- script.js
+++ script.js
@@ -4,32 +4,52 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const instagramBtn = document.getElementById('instagram');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 // const copyBtn = document.getElementById('copy-quote')
+
 
 let apiQuotes = [];
 
+// show loading
+function loading() {
+    loader.hidden =false;
+    quoteContainer.hidden = true;
+}
+
+//  hide loading
+function complete() {
+    if (!loader.hidden) { 
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+
 //show new quote
 function newQuote() {
+    loading();
  //pick a random quote from apiquotes array
  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // check if author field is blank and replace with 'unknown'
+  if(!quote.author) {
+    authorText.textContent = 'Unknown';
+} else {
+    authorText.textContent =quote.author;
+}   
  // check quote length to determine styling
     if(quote.text.length >50) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // set quote hide loader
     quoteText.textContent = quote.text;
-    if(!quote.author) {
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent =quote.author;
-    }
-    
-
+    complete();
 
 }
 // get Quotes from API
 async function getquotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
@@ -63,7 +83,7 @@ function quoteOnInsta(){
 newQuoteBtn.addEventListener('click',newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 instagramBtn.addEventListener('click', quoteOnInsta);
-// copyBtn.addEventListener('click',copyText);
+//  copyBtn.addEventListener('click',copyQuote);
 
 //onload
-getquotes();
+getquotes();  
